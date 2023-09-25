6890b217a928b83a631c7b1fb9860c0395dc46dd
Steven Edwards <steven@stephenwithav.com>
2023-09-25 16:05:17 -0400

    Add generate submenu scaffolding.

--- aangit.el
+++ aangit.el
@@ -28,7 +28,35 @@
    ("n" "new" aangit-menu--ng-new)]
   )
 
+(defun aangit-menu--unimplemented ()
+  (interactive)
+  (message "not yet implemented"))
+
+(transient-define-prefix aangit-menu--generate-submenu ()
+  :value '("--defaults")
+  ["Generate what?"
+   ("a" "Application shell" aangit-menu--unimplemented)
+   ("A" "application in projects" aangit-menu--unimplemented)
+   ("C" "Class" aangit-menu--unimplemented)
+   ("c" "Component" aangit-menu--unimplemented)
+   ("o" "Configuration file" aangit-menu--unimplemented)
+   ("d" "Directive" aangit-menu--unimplemented)
+   ("e" "Enums" aangit-menu--unimplemented)
+   ("g" "Guard" aangit-menu--unimplemented)
+   ("I" "Interceptor" aangit-menu--unimplemented)
+   ("i" "Interface" aangit-menu--unimplemented)
+   ("l" "Library" aangit-menu--unimplemented)
+   ("m" "Module" aangit-menu--unimplemented)
+   ("p" "Pipe" aangit-menu--unimplemented)
+   ("r" "Resolver" aangit-menu--unimplemented)
+   ("s" "Service" aangit-menu--unimplemented)
+   ("S" "Service Worker" aangit-menu--unimplemented)
+   ("w" "Web Worker" aangit-menu--unimplemented)
+   ]
+  )
+
 (transient-define-prefix aangit-menu ()
   [["ng"
     ("n" "new" aangit-menu--new-project)
+    ("g" "generate" aangit-menu--generate-submenu)
     ]])
