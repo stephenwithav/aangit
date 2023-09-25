36f53abae74b10b94f2419adb51dd6d4cacbd32a
Steven Edwards <steven@stephenwithav.com>
2023-09-25 14:18:45 -0400

    The initial interface for ng new works
    aangit-menu--new-project can successfully scaffold a new Angular
    project.
    
    More work, like ensuring all ng new flags are supported, is needed.

--- aangit.el
+++ aangit.el
@@ -1,28 +1,35 @@
-(transient-define-suffix aangit-menu--suffix (&optional args)
-  :key "x"
-  :description "suffix"
-  ;; :transient t
+(transient-define-suffix aangit-menu--ng-new (&optional args)
+  :description "ng new"
   (interactive (list (transient-args transient-current-command)))
-  (message "%s" args)
-  )
+  (let ((dir (car (last (string-split (car (dired-read-dir-and-switches "")) "/"))))
+        (args (string-join args " ")))
+    (if (eq dir "")
+        (message "missing project name")
+      (shell-command (format "ng new %s %s" dir args)))))
 
 (transient-define-argument aangit-menu--new-project-style ()
   :description "Style"
-  :class transient-switches
+  :class transient-option
   :key "-y"
-  :argument-format "--style=%s"
-  :argument-regexp ".+"
+  :argument "--style="
   :choices '("css" "scss" "sass" "less")
   )
 
 (transient-define-prefix aangit-menu--new-project ()
+  :incompatible '(
+                  ("--defaults" "--standalone")
+                  ("--defaults" "--routing")
+                  ("--defaults" "--style")
+                  )
+  :value '("--standalone" "--routing" "--style=css")
   ["Switches"
    ("-d" "Defaults" "--defaults" :class transient-switch)
    ("-s" "Standalone" "--standalone" :class transient-switch)
    ("-r" "Routing" "--routing" :class transient-switch)
-   (aangit-menu--new-project-style)]
+   (aangit-menu--new-project-style)
+   ]
   ["Commands"
-   ("n" "new" aangit-menu--suffix)]
+   ("n" "new" aangit-menu--ng-new)]
   )
 
 (transient-define-prefix aangit-menu ()
