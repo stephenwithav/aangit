7527a366c542cb7b09672597876e83f429ca6b46
Steven Edwards <steven@stephenwithav.com>
2023-11-06 16:15:42 -0500

    Replace f-relative with file-relative-name

--- aangit.el
+++ aangit.el
@@ -40,7 +40,7 @@
 (defun aangit--transient-read-directory-with-no-slash (prompt _initial-input _history)
   "Read a directory and remove trailing slash."
   (string-trim-right
-   (f-relative (read-directory-name prompt))
+   (file-relative-name (read-directory-name prompt))
    "/"))
 
 (transient-define-suffix aangit-menu--ng-add-known-schematic-command (&optional args)
